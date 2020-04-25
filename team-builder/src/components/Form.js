922b205287291f2d7fcc059de1d097e9158ba2ec
spazdrum <spazdrum@gmail.com>
2020-04-24 23:27:31 -0400

    Built markup

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const Form =({ addNewMember }) => {
+    const [member, setMember] = useState({
+        name: "",
+        email: "",
+        role: "",
+    });
+
+    const handleChanges = event => {
+        setMember({
+            ...member,
+            [event.target.name]: event.target.value 
+        })
+    };
+
+    const submit = event => {
+        event.preventDefault()
+        addNewMember(member)
+        setMember({
+            name: "",
+            email: "",
+            role: "",
+        });
+    };
+
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <label htmlFor="name">Name</label>
+                <input onChange={inputChange} type="text" placeholder="name" name="name" id="name" value={member.name} />
+                <label htmlFor="email">E-Mail</label>
+                <input onChange={inputChange} type="email" placeholder="email" name="email" id="email" value={mamber.email} />
+                <label htmlFor="role">Role</label>
+                <select onChange={inputChange} placeholder="role" name="role" id="role" value={member.role}>
+                    <option>Frontend Engineer</option>
+                    <option>Backend Engineer</option>
+                    <option>Designer</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;

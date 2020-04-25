e516fc4e092fa022d475974bd7efe4fba021a519
spazdrum <spazdrum@gmail.com>
2020-04-25 00:11:55 -0400

    created members.js, imported into app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Members from './components/Members';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([{name: "", email: "", role: ""}]);
+  const newMember = (newAddedMemberData) => {
+    setTeamMembers([ ...teamMembers, newAddedMemberData ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form newMember={newMember} />
+      <Members teamMembers={teamMembers} />
     </div>
   );
 }
